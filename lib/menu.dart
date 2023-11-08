d5a4e8d3343be0ccc02e9268e171700df2ae35bd
afsarakhaa <afsarrakha140604@gmail.com>
2023-11-08 08:37:49 +0700

    Selesai Tugas 7 Individu PBP

---
+++ lib/menu.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+class ShopItem {
+  final String name;
+  final IconData icon;
+  final Color color;  // Bonus Poin Tugas 6 Individu PBP (Add Different Color for button)
+
+  ShopItem(this.name, this.icon, this.color); // Tambahkan Color pada constructor
+}
+
+class ShopCard extends StatelessWidget {
+  final ShopItem item;
+
+  const ShopCard(this.item, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15), 
+      ),
+      color: item.color, // Passed Color di sini
+      elevation: 4.0,
+      child: InkWell(
+        onTap: () {
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(SnackBar(content: Text("Kamu telah menekan tombol ${item.name}!")));
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                item.icon,
+                color: Colors.white, 
+                size: 48.0,
+              ),
+              const SizedBox(height: 10),
+              Text(
+                item.name,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.white, 
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  final List<ShopItem> items = [
+    ShopItem("Lihat Item", Icons.checklist, Colors.green), // Add Color di sini
+    ShopItem("Tambah Item", Icons.add_shopping_cart, Colors.blue), // Add Color di sini
+    ShopItem("Logout", Icons.logout, Colors.red),  // Add Color di sini
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Rumah Kaos',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.indigo,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              const Padding(
+                padding:  EdgeInsets.only(top: 20.0, bottom: 20.0),
+                child: Text(
+                  'Rumah Kaos Features',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.indigo,
+                  ),
+                ),
+              ),
+              GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  childAspectRatio: 1.0,
+                ),
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(), // to disable GridView's scrolling
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  return ShopCard(items[index]);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
